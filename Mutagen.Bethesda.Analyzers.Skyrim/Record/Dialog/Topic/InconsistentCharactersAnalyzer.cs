3d3879ae28b46b8db3f72752cd82eb20d55f69fe
Elscrux <nickposer2102@gmail.com>
2025-04-13 16:37:53 +0200

    Add inconsistent characters analyzer for dialog topics

---
+++ Mutagen.Bethesda.Analyzers.Skyrim/Record/Dialog/Topic/InconsistentCharactersAnalyzer.cs
@@ -0,0 +1,34 @@
+﻿using Mutagen.Bethesda.Analyzers.SDK.Analyzers;
+using Mutagen.Bethesda.Analyzers.SDK.Topics;
+using Mutagen.Bethesda.Analyzers.Skyrim.Util;
+using Mutagen.Bethesda.Skyrim;
+using Noggog;
+
+namespace Mutagen.Bethesda.Analyzers.Skyrim.Record.Dialog.Topic;
+
+public class InconsistentCharactersAnalyzer : IIsolatedRecordAnalyzer<IDialogTopicGetter>
+{
+    public static readonly TopicDefinition<string> PromptInconsistentCharacters = MutagenTopicBuilder.DevelopmentTopic(
+            "Prompt Has Inconsistent Characters",
+            Severity.Suggestion)
+        .WithFormatting<string>("Response {0} contains characters which are not usually used in dialog");
+
+    public IEnumerable<TopicDefinition> Topics { get; } = [PromptInconsistentCharacters];
+
+
+    public void AnalyzeRecord(IsolatedRecordAnalyzerParams<IDialogTopicGetter> param)
+    {
+        var dialogResponses = param.Record;
+
+        // Check prompt
+        if (dialogResponses.Name?.String is not null)
+        {
+            InvalidCharactersAnalyzerUtil.CheckInconsistentCharacters(param, dialogResponses.Name.String, PromptInconsistentCharacters);
+        }
+    }
+
+    public IEnumerable<Func<IDialogTopicGetter, object?>> FieldsOfInterest()
+    {
+        yield return x => x.Name;
+    }
+}
