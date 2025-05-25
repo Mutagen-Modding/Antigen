f7126dbb36f5e3752cd5d99c4ca6f315e89f8f17
Elscrux <nickposer2102@gmail.com>
2025-05-25 21:56:57 +0200

    Add leveled list size analyzers

---
+++ Mutagen.Bethesda.Analyzers.Skyrim/Record/LeveledItem/LeveledItemSizeAnalyzer.cs
@@ -0,0 +1,33 @@
+﻿using Mutagen.Bethesda.Analyzers.SDK.Analyzers;
+using Mutagen.Bethesda.Analyzers.SDK.Topics;
+using Mutagen.Bethesda.Skyrim;
+
+namespace Mutagen.Bethesda.Analyzers.Skyrim.Record.LeveledItem;
+
+public class LeveledItemSizeAnalyzer : IIsolatedRecordAnalyzer<ILeveledItemGetter>
+{
+    public static readonly TopicDefinition<int> TooManyEntries = MutagenTopicBuilder.FromDiscussion(
+            362,
+            "Too Many Entries in Leveled Item",
+            Severity.Error)
+        .WithFormatting<int>("Leveled Item has {0} which is more than the maximum of 255 entries");
+
+    public IEnumerable<TopicDefinition> Topics { get; } = [TooManyEntries];
+
+    public void AnalyzeRecord(IsolatedRecordAnalyzerParams<ILeveledItemGetter> param)
+    {
+        var entries = param.Record.Entries;
+        if (entries is null) return;
+
+        if (entries.Count > 255)
+        {
+            param.AddTopic(
+                TooManyEntries.Format(entries.Count));
+        }
+    }
+
+    public IEnumerable<Func<ILeveledItemGetter, object?>> FieldsOfInterest()
+    {
+        yield return x => x.Entries;
+    }
+}
---
+++ Mutagen.Bethesda.Analyzers.Skyrim/Record/LeveledSpell/LeveledSpellSizeAnalyzer.cs
@@ -0,0 +1,33 @@
+﻿using Mutagen.Bethesda.Analyzers.SDK.Analyzers;
+using Mutagen.Bethesda.Analyzers.SDK.Topics;
+using Mutagen.Bethesda.Skyrim;
+
+namespace Mutagen.Bethesda.Analyzers.Skyrim.Record.LeveledSpell;
+
+public class LeveledSpellSizeAnalyzer : IIsolatedRecordAnalyzer<ILeveledSpellGetter>
+{
+    public static readonly TopicDefinition<int> TooManyEntries = MutagenTopicBuilder.FromDiscussion(
+            364,
+            "Too Many Entries in Leveled Spell",
+            Severity.Error)
+        .WithFormatting<int>("Leveled Spell has {0} which is more than the maximum of 255 entries");
+
+    public IEnumerable<TopicDefinition> Topics { get; } = [TooManyEntries];
+
+    public void AnalyzeRecord(IsolatedRecordAnalyzerParams<ILeveledSpellGetter> param)
+    {
+        var entries = param.Record.Entries;
+        if (entries is null) return;
+
+        if (entries.Count > 255)
+        {
+            param.AddTopic(
+                TooManyEntries.Format(entries.Count));
+        }
+    }
+
+    public IEnumerable<Func<ILeveledSpellGetter, object?>> FieldsOfInterest()
+    {
+        yield return x => x.Entries;
+    }
+}
