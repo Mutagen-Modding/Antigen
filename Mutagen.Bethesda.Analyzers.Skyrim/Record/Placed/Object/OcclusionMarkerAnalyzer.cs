afeb458efaabb562d47fa43d6f0e446da9d26b0f
Elscrux <nickposer2102@gmail.com>
2025-06-03 23:51:21 +0200

    Add OcclusionMarkerAnalyzer

---
+++ Mutagen.Bethesda.Analyzers.Skyrim/Record/Placed/Object/OcclusionMarkerAnalyzer.cs
@@ -0,0 +1,50 @@
+﻿using Mutagen.Bethesda.Analyzers.SDK.Analyzers;
+using Mutagen.Bethesda.Analyzers.SDK.Topics;
+using Mutagen.Bethesda.Skyrim;
+
+namespace Mutagen.Bethesda.Analyzers.Skyrim.Record.Placed.Object;
+
+public class OcclusionMarkerAnalyzer : IIsolatedRecordAnalyzer<IPlacedObjectGetter>
+{
+    public static readonly TopicDefinition NoPrimitive = MutagenTopicBuilder.FromDiscussion(
+            389,
+            "Occlusion Marker Without Primitive",
+            Severity.Warning)
+        .WithoutFormatting("Occlusion marker placement has no primitive data");
+
+    public static readonly TopicDefinition OcclusionBoxShouldBePlaneMarker = MutagenTopicBuilder.FromDiscussion(
+            390,
+            "Occlusion Box Should Be Plane Marker",
+            Severity.Warning)
+        .WithoutFormatting("Occlusion marker placement is a box, but half-extent is smaller than 32, so it should be a plane marker instead");
+
+    public IEnumerable<TopicDefinition> Topics { get; } = [NoPrimitive];
+
+    public void AnalyzeRecord(IsolatedRecordAnalyzerParams<IPlacedObjectGetter> param)
+    {
+        var placedObject = param.Record;
+
+        if (placedObject.IsDeleted) return;
+
+        if (placedObject.Base.FormKey != FormKeys.SkyrimSE.Skyrim.Static.PlaneMarker.FormKey) return;
+
+        if (placedObject.Primitive is not {} primitive)
+        {
+            param.AddTopic(
+                NoPrimitive.Format());
+            return;
+        }
+
+        if (primitive.Bounds.X < 32 || primitive.Bounds.Y < 32 || primitive.Bounds.Z < 32)
+        {
+            param.AddTopic(
+                OcclusionBoxShouldBePlaneMarker.Format());
+        }
+    }
+
+    public IEnumerable<Func<IPlacedObjectGetter, object?>> FieldsOfInterest()
+    {
+        yield return x => x.Base;
+        yield return x => x.Primitive?.Bounds;
+    }
+}
