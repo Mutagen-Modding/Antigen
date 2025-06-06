92585c95cfd44b5557ababa59142e2001371c68d
Elscrux <nickposer2102@gmail.com>
2025-06-06 16:46:50 +0200

    Add EncounterZoneAnalyzer

---
+++ Mutagen.Bethesda.Analyzers.Skyrim/Record/Cell/Exterior/EncounterZoneAnalyzer.cs
@@ -0,0 +1,37 @@
+﻿using Mutagen.Bethesda.Analyzers.SDK.Analyzers;
+using Mutagen.Bethesda.Analyzers.SDK.Topics;
+using Mutagen.Bethesda.Plugins;
+using Mutagen.Bethesda.Skyrim;
+
+namespace Mutagen.Bethesda.Analyzers.Skyrim.Record.Cell.Exterior;
+
+public class EncounterZoneAnalyzer : IIsolatedRecordAnalyzer<ICellGetter>
+{
+    public static readonly TopicDefinition<IFormLinkNullableGetter<IEncounterZoneGetter>> HasEncounterZone = MutagenTopicBuilder.FromDiscussion(
+            393,
+            "Exterior Cell Has Encounter Zone",
+            Severity.Warning)
+        .WithFormatting<IFormLinkNullableGetter<IEncounterZoneGetter>>("Cell is exterior cell with an encounter zone: {0}");
+
+    public IEnumerable<TopicDefinition> Topics => [HasEncounterZone];
+
+    public void AnalyzeRecord(IsolatedRecordAnalyzerParams<ICellGetter> param)
+    {
+        var cell = param.Record;
+
+        if (cell.IsDeleted) return;
+
+        if (!cell.IsExteriorCell()) return;
+
+        if (!cell.EncounterZone.IsNull)
+        {
+            param.AddTopic(
+                HasEncounterZone.Format(cell.EncounterZone));
+        }
+    }
+
+    public IEnumerable<Func<ICellGetter, object?>> FieldsOfInterest()
+    {
+        yield return x => x.EncounterZone;
+    }
+}
