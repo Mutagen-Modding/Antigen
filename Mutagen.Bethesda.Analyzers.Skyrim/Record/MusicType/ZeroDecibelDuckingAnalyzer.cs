af0ba039b2288b99d13d9623a92caec044f5e6d7
Elscrux <nickposer2102@gmail.com>
2025-07-25 22:57:49 +0200

    Add Zero Decibel Ducking analyzer

---
+++ Mutagen.Bethesda.Analyzers.Skyrim/Record/MusicType/ZeroDecibelDuckingAnalyzer.cs
@@ -0,0 +1,35 @@
+﻿using Mutagen.Bethesda.Analyzers.SDK.Analyzers;
+using Mutagen.Bethesda.Analyzers.SDK.Topics;
+using Mutagen.Bethesda.Skyrim;
+
+namespace Mutagen.Bethesda.Analyzers.Skyrim.Record.MusicType;
+
+public class ZeroDecibelDuckingAnalyzer : IIsolatedRecordAnalyzer<IMusicTypeGetter>
+{
+    public static readonly TopicDefinition ZeroDecibelDucking = MutagenTopicBuilder.FromDiscussion(
+            402,
+            "Zero Decibel Ducking",
+            Severity.Warning)
+        .WithoutFormatting("Music type has DucksCurrentTrack flag set, but the ducking decibel level is set to 0");
+
+    public IEnumerable<TopicDefinition> Topics { get; } = [ZeroDecibelDucking];
+
+    void IIsolatedRecordAnalyzer<IMusicTypeGetter>.AnalyzeRecord(IsolatedRecordAnalyzerParams<IMusicTypeGetter> param)
+    {
+        var record = param.Record;
+
+        if (record.Flags.HasFlag(Bethesda.Skyrim.MusicType.Flag.DucksCurrentTrack))
+        {
+            if (record.Data is { DuckingDecibel: 0 })
+            {
+                param.AddTopic(ZeroDecibelDucking.Format());
+            }
+        }
+    }
+
+    IEnumerable<Func<IMusicTypeGetter, object?>> IIsolatedRecordAnalyzer<IMusicTypeGetter>.FieldsOfInterest()
+    {
+        yield return x => x.Flags;
+        yield return x => x.Data?.DuckingDecibel;
+    }
+}
