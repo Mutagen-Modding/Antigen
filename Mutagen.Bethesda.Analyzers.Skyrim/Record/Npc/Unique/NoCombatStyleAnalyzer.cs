9a4ea9bee5a3b8fa1e80590f34b44d3a50e87e4e
Elscrux <nickposer2102@gmail.com>
2025-08-06 22:39:40 +0200

    Add NoCombatStyleAnalyzer

---
+++ Mutagen.Bethesda.Analyzers.Skyrim/Record/Npc/Unique/NoCombatStyleAnalyzer.cs
@@ -0,0 +1,34 @@
+﻿using Mutagen.Bethesda.Analyzers.SDK.Analyzers;
+using Mutagen.Bethesda.Analyzers.SDK.Topics;
+using Mutagen.Bethesda.Skyrim;
+
+namespace Mutagen.Bethesda.Analyzers.Skyrim.Record.Npc.Unique;
+
+public class NoCombatStyleAnalyzer : IContextualRecordAnalyzer<INpcGetter>
+{
+    public static readonly TopicDefinition NoCombatStyleFaction = MutagenTopicBuilder.FromDiscussion(
+            404,
+            "Npc has no Combat Style",
+            Severity.None)
+        .WithoutFormatting("Npc has no combat style");
+
+    public IEnumerable<TopicDefinition> Topics { get; } = [NoCombatStyleFaction];
+
+    public void AnalyzeRecord(ContextualRecordAnalyzerParams<INpcGetter> param)
+    {
+        var npc = param.Record;
+        if (!npc.IsEligibleForTest(param.LinkCache)) return;
+
+        if (npc.CombatStyle.IsNull)
+        {
+            param.AddTopic(
+                NoCombatStyleFaction.Format());
+        }
+    }
+
+    public IEnumerable<Func<INpcGetter, object?>> FieldsOfInterest()
+    {
+        yield return x => x.CombatStyle;
+        yield return x => x.Name;
+    }
+}
