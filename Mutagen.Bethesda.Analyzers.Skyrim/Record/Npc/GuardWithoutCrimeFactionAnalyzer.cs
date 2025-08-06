5d65418e572de05dbb2ef891f4144b82b5f5c17f
Elscrux <nickposer2102@gmail.com>
2025-08-07 00:31:48 +0200

    Add GuardWithoutCrimeFactionAnalyzer

---
+++ Mutagen.Bethesda.Analyzers.Skyrim/Record/Npc/GuardWithoutCrimeFactionAnalyzer.cs
@@ -0,0 +1,33 @@
+﻿using Mutagen.Bethesda.Analyzers.SDK.Analyzers;
+using Mutagen.Bethesda.Analyzers.SDK.Topics;
+using Mutagen.Bethesda.Skyrim;
+
+namespace Mutagen.Bethesda.Analyzers.Skyrim.Record.Npc;
+
+public class GuardWithoutCrimeFactionAnalyzer : IContextualRecordAnalyzer<INpcGetter>
+{
+    public static readonly TopicDefinition InvalidInterruptOverridePackageUsage = MutagenTopicBuilder.FromDiscussion(
+            406,
+            "Guard without Crime Faction",
+            Severity.Warning)
+        .WithoutFormatting("Npc has IsGuardFaction but has no crime faction assigned");
+
+    public IEnumerable<TopicDefinition> Topics { get; } = [InvalidInterruptOverridePackageUsage];
+
+    public void AnalyzeRecord(ContextualRecordAnalyzerParams<INpcGetter> param)
+    {
+        var npc = param.Record;
+        if (!npc.CrimeFaction.IsNull) return;
+
+        if (npc.Factions.Any(x => x.Faction.FormKey == FormKeys.SkyrimSE.Skyrim.Faction.IsGuardFaction.FormKey))
+        {
+            param.AddTopic(
+                InvalidInterruptOverridePackageUsage.Format());
+        }
+    }
+
+    public IEnumerable<Func<INpcGetter, object?>> FieldsOfInterest()
+    {
+        yield return x => x.Factions;
+    }
+}
