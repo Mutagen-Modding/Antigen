6d782b846b89624cee205394de31dc8318fd63de
Elscrux <nickposer2102@gmail.com>
2025-08-08 00:18:49 +0200

    Add ReservedPlayerAliasAnalyzer

---
+++ Mutagen.Bethesda.Analyzers.Skyrim/Record/Quest/ReservedPlayerAliasAnalyzer.cs
@@ -0,0 +1,39 @@
+﻿using Mutagen.Bethesda.Analyzers.SDK.Analyzers;
+using Mutagen.Bethesda.Analyzers.SDK.Topics;
+using Mutagen.Bethesda.Skyrim;
+
+namespace Mutagen.Bethesda.Analyzers.Skyrim.Record.Quest;
+
+public class ReservedPlayerAliasAnalyzer : IIsolatedRecordAnalyzer<IQuestGetter>
+{
+    public static readonly TopicDefinition<string> ReservedPlayerAlias = MutagenTopicBuilder.FromDiscussion(
+            420,
+            "Reserved Player Alias",
+            Severity.Error)
+        .WithFormatting<string>("Quest has alias {0} which is filled by the player that reserves the player reference");
+
+    public IEnumerable<TopicDefinition> Topics { get; } = [ReservedPlayerAlias];
+
+    public void AnalyzeRecord(IsolatedRecordAnalyzerParams<IQuestGetter> param)
+    {
+        var quest = param.Record;
+        if (quest.IsDeleted) return;
+
+        foreach (var alias in quest.Aliases)
+        {
+            if (!alias.Flags.HasValue || !alias.Flags.Value.HasFlag(QuestAlias.Flag.ReservesLocationOrReference)) continue;
+
+            if (alias.ForcedReference.FormKey == FormKeys.SkyrimSE.Skyrim.PlayerRef.FormKey
+                || alias.UniqueActor.FormKey == FormKeys.SkyrimSE.Skyrim.Npc.Player.FormKey)
+            {
+                param.AddTopic(
+                    ReservedPlayerAlias.Format(alias.Name ?? alias.ID.ToString()));
+            }
+        }
+    }
+
+    public IEnumerable<Func<IQuestGetter, object?>> FieldsOfInterest()
+    {
+        yield return x => x.Aliases;
+    }
+}
