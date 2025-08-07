0fb3e8216b2d7471ee6889c790ea25a9de3170b6
Elscrux <nickposer2102@gmail.com>
2025-08-07 22:06:34 +0200

    Add FactionRankAnalyzer

---
+++ Mutagen.Bethesda.Analyzers.Skyrim/Record/Faction/FactionRankAnalyzer.cs
@@ -0,0 +1,37 @@
+﻿using Mutagen.Bethesda.Analyzers.SDK.Analyzers;
+using Mutagen.Bethesda.Analyzers.SDK.Topics;
+using Mutagen.Bethesda.Skyrim;
+
+namespace Mutagen.Bethesda.Analyzers.Skyrim.Record.Faction;
+
+public class FactionRankAnalyzer : IIsolatedRecordAnalyzer<IFactionGetter>
+{
+    public static readonly TopicDefinition<int> NoRankName = MutagenTopicBuilder.FromDiscussion(
+            407,
+            "Faction Rank without Title",
+            Severity.Suggestion)
+        .WithFormatting<int>("Faction rank {0} has no title");
+
+    public IEnumerable<TopicDefinition> Topics { get; } = [NoRankName];
+
+    public void AnalyzeRecord(IsolatedRecordAnalyzerParams<IFactionGetter> param)
+    {
+        var faction = param.Record;
+
+        for (var i = 0; i < faction.Ranks.Count; i++)
+        {
+            var rank = faction.Ranks[i];
+            if (rank.Title is null)
+            {
+                param.AddTopic(
+                    NoRankName.Format(i));
+            }
+        }
+    }
+
+    public IEnumerable<Func<IFactionGetter, object?>> FieldsOfInterest()
+    {
+        yield return x => x.Flags;
+        yield return x => x.VendorValues;
+    }
+}
