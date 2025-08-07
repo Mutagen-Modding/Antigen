6acf7a407823da940cf6c5f3025fbc0d03631563
Elscrux <nickposer2102@gmail.com>
2025-08-08 00:22:19 +0200

    Add EventConditionsAnalyzer

---
+++ Mutagen.Bethesda.Analyzers.Skyrim/Record/Quest/EventConditionsAnalyzer.cs
@@ -0,0 +1,36 @@
+﻿using Mutagen.Bethesda.Analyzers.SDK.Analyzers;
+using Mutagen.Bethesda.Analyzers.SDK.Topics;
+using Mutagen.Bethesda.Skyrim;
+
+namespace Mutagen.Bethesda.Analyzers.Skyrim.Record.Quest;
+
+public class EventConditionsAnalyzer : IIsolatedRecordAnalyzer<IQuestGetter>
+{
+    public static readonly TopicDefinition<Condition.Function> QuestAliasEventCondition = MutagenTopicBuilder.FromDiscussion(
+            421,
+            "Event Condition runs on Quest Alias",
+            Severity.Error)
+        .WithFormatting<Condition.Function>("Quest has an event condition with function {0} that runs on a quest alias");
+
+    public IEnumerable<TopicDefinition> Topics { get; } = [QuestAliasEventCondition];
+
+    public void AnalyzeRecord(IsolatedRecordAnalyzerParams<IQuestGetter> param)
+    {
+        var quest = param.Record;
+        if (quest.IsDeleted) return;
+
+        foreach (var condition in quest.EventConditions)
+        {
+            if (condition.Data.RunOnType == Condition.RunOnType.QuestAlias)
+            {
+                param.AddTopic(
+                    QuestAliasEventCondition.Format(condition.Data.Function));
+            }
+        }
+    }
+
+    public IEnumerable<Func<IQuestGetter, object?>> FieldsOfInterest()
+    {
+        yield return x => x.Aliases;
+    }
+}
