75d3927a50d083878a47cc20665e68e7d661938c
Elscrux <nickposer2102@gmail.com>
2025-08-10 19:16:29 +0200

    Add TODO

--- Mutagen.Bethesda.Analyzers.Skyrim/Extensions/FurnitureExtensions.cs
+++ Mutagen.Bethesda.Analyzers.Skyrim/Extensions/FurnitureExtensions.cs
@@ -6,6 +6,7 @@ public static class FurnitureExtensions
 {
     public static bool IsBed(this IFurnitureGetter furniture)
     {
+        // TODO: potentially replace with check on nif file to see if the animation type used on the furniture entry node is sleep instead
         return furniture.EditorID is not null && furniture.EditorID.Contains("bed", StringComparison.OrdinalIgnoreCase);
     }
 }
