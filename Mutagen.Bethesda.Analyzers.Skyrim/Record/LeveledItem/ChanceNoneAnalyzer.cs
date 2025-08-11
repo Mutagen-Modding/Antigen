568eee1d8ea6da8b1a12ec62e2ec6c48e11e5942
Elscrux <nickposer2102@gmail.com>
2025-08-12 00:44:20 +0200

    Add ChanceNoneAnalyzer

---
+++ Mutagen.Bethesda.Analyzers.Skyrim/Record/LeveledItem/ChanceNoneAnalyzer.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.RegularExpressions;
+using Mutagen.Bethesda.Analyzers.SDK.Analyzers;
+using Mutagen.Bethesda.Analyzers.SDK.Topics;
+using Mutagen.Bethesda.Skyrim;
+using Noggog;
+
+namespace Mutagen.Bethesda.Analyzers.Skyrim.Record.LeveledItem;
+
+public partial class ChanceNoneAnalyzer : IIsolatedRecordAnalyzer<ILeveledItemGetter>
+{
+    [GeneratedRegex(@"(\d+)$")]
+    public static partial Regex ChanceEditorIDRegex { get; }
+
+    public static readonly TopicDefinition<Percent, int> InvalidChanceNoneEditorID = MutagenTopicBuilder.FromDiscussion(
+            471,
+            "Invalid Chance in Editor ID",
+            Severity.Error)
+        .WithFormatting<Percent, int>("Leveled Item has a Chance None of {0} but the EditorID which should end with the Chance ends with '{1}'");
+
+    public IEnumerable<TopicDefinition> Topics { get; } = [InvalidChanceNoneEditorID];
+
+    public void AnalyzeRecord(IsolatedRecordAnalyzerParams<ILeveledItemGetter> param)
+    {
+        var leveledItem = param.Record;
+        if (leveledItem.IsDeleted) return;
+
+        if (leveledItem.ChanceNone == Percent.Zero) return;
+        if (leveledItem.EditorID is null) return;
+
+        var match = ChanceEditorIDRegex.Match(leveledItem.EditorID);
+        if (!match.Success) return;
+
+        if (!int.TryParse(match.Groups[1].Value, out var editorIdChance)) return;
+
+        if (Math.Abs(editorIdChance + leveledItem.ChanceNone - 1) < 0.001)
+        {
+            param.AddTopic(
+                InvalidChanceNoneEditorID.Format(leveledItem.ChanceNone, editorIdChance));
+        }
+    }
+
+    public IEnumerable<Func<ILeveledItemGetter, object?>> FieldsOfInterest()
+    {
+        yield return x => x.ChanceNone;
+        yield return x => x.EditorID;
+    }
+}
