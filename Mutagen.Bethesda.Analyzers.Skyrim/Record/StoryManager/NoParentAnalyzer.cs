1a288616486127665f7d0475a44605ea7699fbf5
Elscrux <nickposer2102@gmail.com>
2025-08-15 20:24:09 +0200

    Add NoParentAnalyzer

---
+++ Mutagen.Bethesda.Analyzers.Skyrim/Record/StoryManager/NoParentAnalyzer.cs
@@ -0,0 +1,33 @@
+﻿using Mutagen.Bethesda.Analyzers.SDK.Analyzers;
+using Mutagen.Bethesda.Analyzers.SDK.Topics;
+using Mutagen.Bethesda.Skyrim;
+
+namespace Mutagen.Bethesda.Analyzers.Skyrim.Record.StoryManager;
+
+public class NoParentAnalyzer : IIsolatedRecordAnalyzer<IAStoryManagerNodeGetter>
+{
+    public static readonly TopicDefinition NoParent = MutagenTopicBuilder.FromDiscussion(
+            419,
+            "No Parent",
+            Severity.Error)
+        .WithoutFormatting("Node has no parent");
+
+    public IEnumerable<TopicDefinition> Topics { get; } = [NoParent];
+
+    public void AnalyzeRecord(IsolatedRecordAnalyzerParams<IAStoryManagerNodeGetter> param)
+    {
+        var branchNode = param.Record;
+        if (branchNode.IsDeleted) return;
+
+        if (branchNode.Parent.IsNull && branchNode.FormKey != FormKeys.SkyrimSE.Skyrim.AStoryManagerNode.Root.FormKey)
+        {
+            param.AddTopic(
+                NoParent.Format());
+        }
+    }
+
+    public IEnumerable<Func<IAStoryManagerNodeGetter, object?>> FieldsOfInterest()
+    {
+        yield return x => x.Parent;
+    }
+}
