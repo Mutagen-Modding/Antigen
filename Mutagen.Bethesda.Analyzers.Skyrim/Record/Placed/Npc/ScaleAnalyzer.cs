6f7616b03932711d96dbd3ebafabd8644afc5f83
Elscrux <nickposer2102@gmail.com>
2025-08-29 11:28:18 +0200

    Add npc ScaleAnalyzer

---
+++ Mutagen.Bethesda.Analyzers.Skyrim/Record/Placed/Npc/ScaleAnalyzer.cs
@@ -0,0 +1,41 @@
+﻿using Mutagen.Bethesda.Analyzers.SDK.Analyzers;
+using Mutagen.Bethesda.Analyzers.SDK.Topics;
+using Mutagen.Bethesda.Plugins;
+using Mutagen.Bethesda.Skyrim;
+
+namespace Mutagen.Bethesda.Analyzers.Skyrim.Record.Placed.Npc;
+
+public class ScaleAnalyzer : IContextualRecordAnalyzer<IPlacedNpcGetter>
+{
+    public static readonly TopicDefinition<IFormLinkNullableGetter<INpcGetter>, float?> ScaleNotOne = MutagenTopicBuilder.FromDiscussion(
+            486,
+            "Placed NPC Scale Not One",
+            Severity.Warning)
+        .WithFormatting<IFormLinkNullableGetter<INpcGetter>, float?>("Npc placement {0} at  with scale {1} is not one");
+
+    public IEnumerable<TopicDefinition> Topics { get; } = [ScaleNotOne];
+
+    public void AnalyzeRecord(ContextualRecordAnalyzerParams<IPlacedNpcGetter> param)
+    {
+        var placedNpc = param.Record;
+
+        if (placedNpc.IsDeleted) return;
+
+        var scaleNullable = placedNpc.Scale;
+        if (scaleNullable is null) return;
+
+        var scale = scaleNullable.Value;
+
+        if (Math.Abs(scale - 1) > 0.01)
+        {
+            param.AddTopic(
+                ScaleNotOne.Format(placedNpc.Base, placedNpc.Scale));
+        }
+    }
+
+    public IEnumerable<Func<IPlacedNpcGetter, object?>> FieldsOfInterest()
+    {
+        yield return x => x.Scale;
+        yield return x => x.Base;
+    }
+}
