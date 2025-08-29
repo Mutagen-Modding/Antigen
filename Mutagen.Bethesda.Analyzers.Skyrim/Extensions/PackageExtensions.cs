179cd1113cb01b270f1e6b6a95638d4c0b3ee977
Elscrux <nickposer2102@gmail.com>
2025-08-29 11:28:09 +0200

    Add PackageAnalyzer

---
+++ Mutagen.Bethesda.Analyzers.Skyrim/Extensions/PackageExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Mutagen.Bethesda.Plugins.Cache;
+using Mutagen.Bethesda.Skyrim;
+
+namespace Mutagen.Bethesda.Analyzers.Skyrim.Extensions;
+
+public static class PackageExtensions
+{
+    public static string? GetPackageDataName(this IPackageGetter package, sbyte key, ILinkCache linkCache)
+    {
+        var template = package.PackageTemplate.TryResolve(linkCache);
+        if (template is not null && template.Data.TryGetValue(key, out var templateData))
+        {
+            return templateData.Name;
+        }
+
+        return null;
+    }
+}
