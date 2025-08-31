054ddcd32b92791043dbdc4d0c63244a25066254
Elscrux <nickposer2102@gmail.com>
2025-09-01 01:20:35 +0200

    Add usage cache used to cache usage cache results

---
+++ Mutagen.Bethesda.Analyzers.Skyrim/Util/UsageCacheUtil.cs
@@ -0,0 +1,24 @@
+﻿using Mutagen.Bethesda.Plugins.Cache;
+using Mutagen.Bethesda.Plugins.Cache.Internals.Implementations;
+
+namespace Mutagen.Bethesda.Analyzers.Skyrim.Util;
+
+public static class UsageCacheUtil
+{
+    private static readonly Lock Lock = new();
+    private static ImmutableLoadOrderLinkUsageCache? _usageCache;
+
+    public static ImmutableLoadOrderLinkUsageCache GetUsageCache(ILinkCache linkCache)
+    {
+        // TODO: Replace with something that is updated when the link cache is updated
+        lock (Lock)
+        {
+            if (_usageCache is null)
+            {
+                _usageCache = new ImmutableLoadOrderLinkUsageCache(linkCache);
+            }
+        }
+
+        return _usageCache;
+    }
+}
