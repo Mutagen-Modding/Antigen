811c3c7bab165fb1cdc472cf68a4628207a1de10
Elscrux <nickposer2102@gmail.com>
2025-09-01 01:26:07 +0200

    Add CollectionReportHandler

---
+++ Mutagen.Bethesda.Analyzers.Engine/Reporting/Handlers/CollectionReportHandler.cs
@@ -0,0 +1,38 @@
+﻿using Mutagen.Bethesda.Analyzers.SDK.Drops;
+using Mutagen.Bethesda.Analyzers.SDK.Topics;
+using Mutagen.Bethesda.Plugins;
+using Mutagen.Bethesda.Plugins.Records;
+
+namespace Mutagen.Bethesda.Analyzers.Reporting.Handlers;
+
+public class AnalyzerResult
+{
+    public required Topic Topic { get; init; }
+    public required IMajorRecordIdentifierGetter Record { get; init; }
+    public required ModKey ModKey { get; init; }
+}
+
+public class CollectionReportHandler : IReportHandler
+{
+    public List<AnalyzerResult> Results { get; } = [];
+
+    public void Dropoff(ReportContextParameters parameters, ModKey sourceMod, IMajorRecordIdentifierGetter majorRecord, Topic topic)
+    {
+        Results.Add(new AnalyzerResult
+        {
+            Topic = topic,
+            Record = majorRecord,
+            ModKey = sourceMod,
+        });
+    }
+
+    public void Dropoff(ReportContextParameters parameters, Topic topic)
+    {
+        Results.Add(new AnalyzerResult
+        {
+            Topic = topic,
+            Record = null!,
+            ModKey = ModKey.Null,
+        });
+    }
+}
