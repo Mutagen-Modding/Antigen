8feb6565c116a15de5dbc5f64cdf0105378a3e1e
Elscrux <nickposer2102@gmail.com>
2025-09-03 18:22:46 +0200

    Add PackageTargetsNoObject

--- Mutagen.Bethesda.Analyzers.Skyrim/Extensions/PackageExtensions.cs
+++ Mutagen.Bethesda.Analyzers.Skyrim/Extensions/PackageExtensions.cs
@@ -7,12 +7,76 @@ public static class PackageExtensions
 {
     public static string? GetPackageDataName(this IPackageGetter package, sbyte key, ILinkCache linkCache)
     {
+        // Check template
         var template = package.PackageTemplate.TryResolve(linkCache);
         if (template is not null && template.Data.TryGetValue(key, out var templateData))
         {
             return templateData.Name;
         }
 
+        // Check package itself
+        if (package.Data.TryGetValue(key, out var data))
+        {
+            return data.Name;
+        }
+
         return null;
+    }
+
+    public static IEnumerable<IPackageBranchGetter> GetDataUsageInProcedure(this IReadOnlyList<IPackageBranchGetter> branches, byte dataIndex)
+    {
+        foreach (var branch in branches)
+        {
+            if (!branch.BranchType.Equals("Procedure", StringComparison.OrdinalIgnoreCase)) continue;
+
+            if (branch.DataInputIndices.Contains(dataIndex))
+            {
+                yield return branch;
+            }
+        }
+    }
+
+    public record ProcedureTreeNode
+    {
+        public required IPackageBranchGetter Branch { get; init; }
+        public List<ProcedureTreeNode> Children { get; init; } = [];
+        public ProcedureTreeNode? Parent { get; init; }
+
+        public ProcedureTreeNode? FindNode(IPackageBranchGetter branch)
+        {
+            if (Branch.Equals(branch)) return this;
+
+            return Children.Select(child => child.FindNode(branch)).FirstOrDefault();
+        }
+    }
+
+    public static ProcedureTreeNode BuildProcedureTree(this IEnumerator<IPackageBranchGetter> branches, ProcedureTreeNode? parent = null)
+    {
+        var branch = branches.Current;
+
+        var node = new ProcedureTreeNode
+        {
+            Branch = branch,
+            Parent = parent,
+        };
+
+        if (!branches.MoveNext() || branch.Root is null)
+        {
+            return node;
+        }
+
+        for (var i = 0; i < branch.Root.BranchCount; i++)
+        {
+            node.Children.Add(BuildProcedureTree(branches, node));
+        }
+
+        return node;
+    }
+
+    public static ProcedureTreeNode BuildProcedureTree(this IReadOnlyList<IPackageBranchGetter> branches)
+    {
+        using var enumerator = branches.GetEnumerator();
+        enumerator.MoveNext();
+        return enumerator.BuildProcedureTree();
     }
 }
---
+++ Mutagen.Bethesda.Analyzers.Skyrim/Record/Package/PackageDataAnalyzer.cs
@@ -0,0 +1,184 @@
+﻿using Mutagen.Bethesda.Analyzers.SDK.Analyzers;
+using Mutagen.Bethesda.Analyzers.SDK.Topics;
+using Mutagen.Bethesda.Plugins;
+using Mutagen.Bethesda.Skyrim;
+
+namespace Mutagen.Bethesda.Analyzers.Skyrim.Record.Package;
+
+public class PackageDataAnalyzer : IContextualRecordAnalyzer<IPackageGetter>
+{
+    public static readonly TopicDefinition<string> PackageWithoutOwningQuestReferencingQuestAlias = MutagenTopicBuilder.FromDiscussion(
+            488,
+            "Package Without Owning Quest Referencing Quest Alias",
+            Severity.Error)
+        .WithFormatting<string>("Package without owning quest data '{0}' references quest alias");
+
+    public static readonly TopicDefinition<string, IQuestGetter> PackageReferencingMissingQuestAlias = MutagenTopicBuilder.FromDiscussion(
+            489,
+            "Package Referencing Missing Quest Alias",
+            Severity.Error)
+        .WithFormatting<string, IQuestGetter>("Package data '{0}' references quest alias missing in quest {1}");
+
+    public static readonly TopicDefinition<string> PackageTargetsNoObject = MutagenTopicBuilder.FromDiscussion(
+            496,
+            "Package Targets No Object",
+            Severity.Warning)
+        .WithFormatting<string>("Package data '{0}' targets no object");
+
+    public IEnumerable<TopicDefinition> Topics { get; } = [PackageWithoutOwningQuestReferencingQuestAlias, PackageReferencingMissingQuestAlias];
+
+    public void AnalyzeRecord(ContextualRecordAnalyzerParams<IPackageGetter> param)
+    {
+        var package = param.Record;
+        if (package.IsDeleted) return;
+
+        foreach (var (key, data) in package.Data)
+        {
+            switch (data)
+            {
+                case IPackageDataLocationGetter packageDataLocation:
+                {
+                    switch (packageDataLocation.Location.Target)
+                    {
+                        case ILocationCellGetter locationCell:
+                        {
+                            CheckNull(locationCell.Link);
+                            break;
+                        }
+                        case ILocationFallbackGetter locationFallback:
+                        {
+                            if (locationFallback.Type is LocationTargetRadius.LocationType.AliasForReference or LocationTargetRadius.LocationType.AliasForLocation)
+                            {
+                                CheckInvalidAlias(locationFallback.Data);
+                            }
+                            break;
+                        }
+                        case ILocationObjectIdGetter locationObjectId:
+                        {
+                            CheckNull(locationObjectId.Link);
+                            break;
+                        }
+                        case ILocationObjectTypeGetter locationObjectType:
+                        {
+                            CheckNone(locationObjectType.Type);
+                            break;
+                        }
+                        case ILocationTargetGetter locationTarget:
+                        {
+                            CheckNull(locationTarget.Link);
+                            break;
+                        }
+                    }
+                    break;
+                }
+                case IPackageDataTargetGetter dataTarget:
+                {
+                    switch (dataTarget.Target)
+                    {
+                        case IPackageTargetAliasGetter targetAlias:
+                            CheckInvalidAlias(targetAlias.Alias);
+                            break;
+                        case IPackageTargetObjectIDGetter packageTargetObjectID:
+                            CheckNull(packageTargetObjectID.Reference);
+                            break;
+                        case IPackageTargetObjectTypeGetter packageTargetObjectType:
+                            CheckNone(packageTargetObjectType.Type);
+                            break;
+                        case IPackageTargetSpecificReferenceGetter packageTargetSpecificReference:
+                            CheckNull(packageTargetSpecificReference.Reference);
+                            break;
+                    }
+
+                    break;
+                }
+            }
+
+            void CheckNull(IFormLinkGetter formLink)
+            {
+                if (package.Type == Bethesda.Skyrim.Package.Types.PackageTemplate) return;
+
+                if (formLink.IsNull)
+                {
+                    var packageBranchGetters = GetBranchesNotGuardedByNullCondition(key).ToList();
+                    if (packageBranchGetters.Count == 0) return;
+
+                    param.AddTopic(
+                        PackageTargetsNoObject.Format(package.GetPackageDataName(key, param.LinkCache) ?? key.ToString()));
+                }
+            }
+
+            void CheckNone(TargetObjectType type)
+            {
+                if (package.Type == Bethesda.Skyrim.Package.Types.PackageTemplate) return;
+
+                if (type == TargetObjectType.None)
+                {
+                    var unguardedBranches = GetBranchesNotGuardedByNullCondition(key).ToList();
+                    if (unguardedBranches.Count == 0) return;
+
+                    // Ignore data called "EmptyTarget" like it's used in PatrolAndHunt
+                    var name = package.GetPackageDataName(key, param.LinkCache);
+                    if (string.Equals(name, "EmptyTarget", StringComparison.OrdinalIgnoreCase)) return;
+
+                    param.AddTopic(
+                        PackageTargetsNoObject.Format(name ?? key.ToString()));
+                }
+            }
+
+            void CheckInvalidAlias(int aliasIndex)
+            {
+                if (package.OwnerQuest.IsNull)
+                {
+                    param.AddTopic(
+                        PackageWithoutOwningQuestReferencingQuestAlias.Format(package.GetPackageDataName(key, param.LinkCache) ?? key.ToString()));
+                }
+                else
+                {
+                    var quest = package.OwnerQuest.TryResolve(param.LinkCache);
+                    if (quest is null) return;
+                    if (quest.HasAlias((uint)aliasIndex)) return;
+
+                    param.AddTopic(
+                        PackageReferencingMissingQuestAlias.Format(package.GetPackageDataName(key, param.LinkCache) ?? key.ToString(), quest));
+                }
+            }
+        }
+
+        IEnumerable<IPackageBranchGetter> GetBranchesNotGuardedByNullCondition(sbyte key)
+        {
+            var template = package.PackageTemplate.TryResolve(param.LinkCache);
+            var branches = (template is null ? package.ProcedureTree : template.ProcedureTree);
+            var procedureTree = branches.BuildProcedureTree();
+
+            foreach (var procedureUsage in branches.GetDataUsageInProcedure((byte) key))
+            {
+                var procedureTreeNode = procedureTree.FindNode(procedureUsage);
+                var currentParent = procedureTreeNode?.Parent;
+                if (currentParent is null) continue;
+
+                var foundCondition = false;
+                foreach (var condition in currentParent.Branch.Conditions)
+                {
+                    if (condition.Data is IIsNullPackageDataConditionDataGetter isNullPackageDataCondition
+                        && isNullPackageDataCondition.PackageDataIndex == key)
+                    {
+                        // We found a condition in a parent that checks for null, so this is probably intentional
+                        foundCondition = true;
+                        break;
+                    }
+                }
+
+                if (!foundCondition)
+                {
+                    yield return procedureUsage;
+                }
+            }
+        }
+    }
+
+    public IEnumerable<Func<IPackageGetter, object?>> FieldsOfInterest()
+    {
+        yield return x => x.OwnerQuest;
+        yield return x => x.Data;
+    }
+}
