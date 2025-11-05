27e520e680f5c02e370bc4006c7f153f8cb3d55e
Elscrux <nickposer2102@gmail.com>
2025-11-05 21:35:43 +0100

    Make contextual analyzers contextual record analyzers where possible

---
+++ Mutagen.Bethesda.Analyzers.Skyrim/Record/Cell/Interior/UnownedBedAnalyzer.cs
@@ -0,0 +1,49 @@
+﻿using Mutagen.Bethesda.Analyzers.SDK.Analyzers;
+using Mutagen.Bethesda.Analyzers.SDK.Topics;
+using Mutagen.Bethesda.Skyrim;
+
+namespace Mutagen.Bethesda.Analyzers.Skyrim.Record.Cell.Interior;
+
+public class UnownedBedAnalyzer : IContextualRecordAnalyzer<ICellGetter>
+{
+    public static readonly TopicDefinition<IPlacedObjectGetter, ICellGetter> UnownedBed = MutagenTopicBuilder.FromDiscussion(
+            209,
+            "Unowned Bed in Owned Cell",
+            Severity.Suggestion)
+        .WithFormatting<IPlacedObjectGetter, ICellGetter>("Unowned bed placement {0} in owned cell {1}");
+
+    public IEnumerable<TopicDefinition> Topics { get; } = [UnownedBed];
+
+    public void AnalyzeRecord(ContextualRecordAnalyzerParams<ICellGetter> param)
+    {
+        var cell = param.Record;
+        if (cell.IsExteriorCell()) return;
+
+        // If the cell is public or unowned, the bed can be unowned too
+        if (cell.IsPublic() || cell.Owner.IsNull) return;
+
+        foreach (var placedObject in cell.GetAllPlaced(param.LinkCache).OfType<IPlacedObjectGetter>())
+        {
+            if (placedObject.IsDeleted) continue;
+
+            // Owned beds are not a problem
+            if (!placedObject.Owner.IsNull) continue;
+
+            if (!param.LinkCache.TryResolve<IFurnitureGetter>(placedObject.Base.FormKey, out var furniture)) continue;
+            if (!furniture.IsBed()) continue;
+
+            var context = param.LinkCache.ResolveSimpleContext<IPlacedObjectGetter>(placedObject.FormKey);
+            param.AddTopic(
+                UnownedBed.Format(placedObject, cell)
+            );
+        }
+    }
+
+    public IEnumerable<Func<ICellGetter, object?>> FieldsOfInterest()
+    {
+        yield return x => x.Flags;
+        yield return x => x.Owner;
+        yield return x => x.Temporary;
+        yield return x => x.Persistent;
+    }
+}
---
+++ Mutagen.Bethesda.Analyzers.Skyrim/Record/Cell/Interior/UnownedWorkMarkerAnalyzer.cs
@@ -0,0 +1,51 @@
+﻿using Mutagen.Bethesda.Analyzers.SDK.Analyzers;
+using Mutagen.Bethesda.Analyzers.SDK.Topics;
+using Mutagen.Bethesda.Plugins;
+using Mutagen.Bethesda.Skyrim;
+
+namespace Mutagen.Bethesda.Analyzers.Skyrim.Record.Cell.Interior;
+
+public class UnownedWorkMarkerAnalyzer : IContextualRecordAnalyzer<ICellGetter>
+{
+    public static readonly TopicDefinition<IPlacedObjectGetter, ICellGetter> UnownedBed = MutagenTopicBuilder.FromDiscussion(
+            210,
+            "Unowned Work Marker in Owned Cell",
+            Severity.Suggestion)
+        .WithFormatting<IPlacedObjectGetter, ICellGetter>("Unowned work marker {0} in owned cell {1}");
+
+    public IEnumerable<TopicDefinition> Topics { get; } = [UnownedBed];
+
+    private static readonly HashSet<FormKey> WorkMarkers =
+    [
+        FormKeys.SkyrimSE.Skyrim.IdleMarker.SweepIdleMarker.FormKey,
+        FormKeys.SkyrimSE.Skyrim.IdleMarker.IdleFarmingMarker.FormKey,
+        FormKeys.SkyrimSE.Skyrim.Furniture.CounterBarLeanMarker.FormKey
+    ];
+
+    public void AnalyzeRecord(ContextualRecordAnalyzerParams<ICellGetter> param)
+    {
+        var cell = param.Record;
+        if (cell.IsExteriorCell()) return;
+        if (cell.Owner.IsNull) return;
+
+        foreach (var placedObject in cell.GetAllPlaced(param.LinkCache).OfType<IPlacedObjectGetter>())
+        {
+            if (placedObject.IsDeleted) continue;
+
+            if (WorkMarkers.Contains(placedObject.Base.FormKey))
+            {
+                param.AddTopic(
+                    UnownedBed.Format(placedObject, cell));
+            }
+        }
+
+    }
+
+    public IEnumerable<Func<ICellGetter, object?>> FieldsOfInterest()
+    {
+        yield return x => x.Owner;
+        yield return x => x.Flags;
+        yield return x => x.Temporary;
+        yield return x => x.Persistent;
+    }
+}
