0db98d28c928f9113e87746aa1d009b42f40ee01
Kieran <kieranknowles11@hotmail.co.uk>
2026-03-03 23:02:40 +0000

    Responses and shared response data analyzer
    Closes #539

---
+++ Mutagen.Bethesda.Analyzers.Skyrim/Record/Dialog/Responses/ResponsesAndResponseDataAnalyzer.cs
@@ -0,0 +1,29 @@
+using Mutagen.Bethesda.Analyzers.SDK.Analyzers;
+using Mutagen.Bethesda.Analyzers.SDK.Topics;
+using Mutagen.Bethesda.Skyrim;
+
+namespace Mutagen.Bethesda.Analyzers.Skyrim.Record.Dialog.Responses;
+
+public class ResponsesAndResponseDataAnalyzer : IIsolatedRecordAnalyzer<IDialogResponsesGetter>
+{
+    public static readonly TopicDefinition ResponsesAndResponseData = MutagenTopicBuilder.DevelopmentTopic(
+        "Dialog Responses Contains Shared ResponseData and Responses",
+        Severity.Warning
+    );
+
+    public IEnumerable<TopicDefinition> Topics { get; } = [ResponsesAndResponseData];
+
+    public void AnalyzeRecord(IsolatedRecordAnalyzerParams<IDialogResponsesGetter> param)
+    {
+        if (param.Record.Responses.Count > 0 && !param.Record.ResponseData.IsNull)
+        {
+            param.AddTopic(ResponsesAndResponseData.Format());
+        }
+    }
+
+    public IEnumerable<Func<IDialogResponsesGetter, object?>> FieldsOfInterest()
+    {
+        yield return x => x.Responses;
+        yield return x => x.ResponseData;
+    }
+}
