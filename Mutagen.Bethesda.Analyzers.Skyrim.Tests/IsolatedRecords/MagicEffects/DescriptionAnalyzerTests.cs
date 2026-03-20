acb9e43c9e0431e34aeef8c8496d521ffc960aeb
FaffiSalus <pstrubig@icloud.com>
2026-03-20 18:18:44 +0100

    Added Language in Topic and improved Logic for multiple consecutive %

--- Mutagen.Bethesda.Analyzers.Skyrim.Tests/IsolatedRecords/MagicEffects/DescriptionAnalyzerTests.cs
+++ Mutagen.Bethesda.Analyzers.Skyrim.Tests/IsolatedRecords/MagicEffects/DescriptionAnalyzerTests.cs
@@ -10,24 +10,27 @@ namespace Mutagen.Bethesda.Analyzers.Skyrim.Tests.IsolatedRecords.MagicEffects;
 public class DescriptionAnalyzerTests
 {
     [Theory, MutagenModAutoData]
-    public void DetectsSinglePercent(
+    public void DetectsIncorrectPercentageUsage(
         IsolatedRecordTestFixture<DescriptionAnalyzer, MagicEffect, IMagicEffectGetter> fixture)
     {
         fixture.Run(
             prepForError: rec => rec.Description = new TranslatedString(Language.English,
                 new KeyValuePair<Language, string>(Language.English, "Deals 50% damage"),
-                new KeyValuePair<Language, string>(Language.French, "Inflige 50% de dégâts"),
+                new KeyValuePair<Language, string>(Language.French, "Inflige 50%%%%%% de dégâts"),
                 new KeyValuePair<Language, string>(Language.Danish, "50%"),
-                new KeyValuePair<Language, string>(Language.German, "%50"),
-                new KeyValuePair<Language, string>(Language.Arabic, "%")),
+                new KeyValuePair<Language, string>(Language.German, "%50%"),
+                new KeyValuePair<Language, string>(Language.Arabic, "%"),
+                new KeyValuePair<Language, string>(Language.Chinese, "5%%%")),
             prepForFix: rec => rec.Description = new TranslatedString(Language.English,
                 new KeyValuePair<Language, string>(Language.English, "Deals 50%% damage"),
                 new KeyValuePair<Language, string>(Language.French, "Inflige 50%% de dégâts"),
                 new KeyValuePair<Language, string>(Language.Danish, "50%%"),
-                new KeyValuePair<Language, string>(Language.German, "%%50"),
-                new KeyValuePair<Language, string>(Language.Arabic, "%%")),
+                new KeyValuePair<Language, string>(Language.German, "%%50%%"),
+                new KeyValuePair<Language, string>(Language.Arabic, "%%"),
+                new KeyValuePair<Language, string>(Language.Chinese, "5%%")),
             new[]
             {
+                DescriptionAnalyzer.MagicEffectDescriptionList,
                 DescriptionAnalyzer.MagicEffectDescriptionList,
                 DescriptionAnalyzer.MagicEffectDescriptionList,
                 DescriptionAnalyzer.MagicEffectDescriptionList,
