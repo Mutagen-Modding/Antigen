368ec83db08fb121e10c42531f95d93cf7a29460
Kieran <kieranknowles11@hotmail.co.uk>
2026-07-31 21:54:47 +0100

    Add exposure test for condition analyzer

--- Mutagen.Bethesda.Analyzers.Skyrim.Tests/TopicExposureTest.cs
+++ Mutagen.Bethesda.Analyzers.Skyrim.Tests/TopicExposureTest.cs
@@ -12,7 +12,7 @@ namespace Mutagen.Bethesda.Analyzers.Skyrim.Tests;
 
 public class TopicExposureTest
 {
-    class TopicExposureTestData : TheoryData<IAnalyzer>
+    class TopicExposureTestData<T> : TheoryData<T>
     {
         public TopicExposureTestData()
         {
@@ -22,25 +22,36 @@ public class TopicExposureTest
             }
         }
 
-        static IEnumerable<IAnalyzer> GetAllAnalyzers()
+        static IEnumerable<T> GetAllAnalyzers()
         {
             var builder = new ContainerBuilder();
             builder.RegisterInstance(GameConstants.Get(GameRelease.SkyrimSE)).As<GameConstants>();
             builder.RegisterModule<SkyrimAnalyzerModule>();
             builder.RegisterModule<TestModule>();
             var container = builder.Build();
-            return container.Resolve<IEnumerable<IAnalyzer>>();
+            return container.Resolve<IEnumerable<T>>();
         }
     }
 
 
-    [Theory, ClassData(typeof(TopicExposureTestData))]
+    [Theory, ClassData(typeof(TopicExposureTestData<IAnalyzer>))]
     public void AllTopicsExposed(IAnalyzer analyzer)
     {
         // Special case: ConditionAnalyzer gets its topics from IConditionAnalyzer types
         if (analyzer is ConditionAnalyzer)
             return;
 
+        var reflectionTopics = analyzer.GetType().GetFields()
+            .Where(f => f.IsStatic)
+            .Select(f => f.GetValue(analyzer))
+            .Where(f => f is TopicDefinition);
+
+        reflectionTopics.ShouldBe(analyzer.Topics, ignoreOrder: true, customMessage: analyzer.GetType().FullName);
+    }
+
+    [Theory, ClassData(typeof(TopicExposureTestData<IConditionAnalyzer>))]
+    public void AllConditionTopicsExposed(IConditionAnalyzer analyzer)
+    {
         var reflectionTopics = analyzer.GetType().GetFields()
             .Where(f => f.IsStatic)
             .Select(f => f.GetValue(analyzer))
