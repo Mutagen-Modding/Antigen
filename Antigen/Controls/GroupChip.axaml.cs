4ab77ccdb5db78b58fb59a57becfba44d897d07e
Justin Swanson <justin.c.swanson@gmail.com>
2026-08-04 17:54:54 -0500

    GroupChip properties misregistered

--- Antigen/Controls/GroupChip.axaml.cs
+++ Antigen/Controls/GroupChip.axaml.cs
@@ -7,8 +7,8 @@ namespace Antigen.Controls;
 
 public partial class GroupChip : UserControl
 {
-    public static readonly StyledProperty<ICommand> RemoveCommandProperty = AvaloniaProperty.Register<AnalyzerDashboard, ICommand>(nameof(RemoveCommand));
-    public static readonly StyledProperty<string> LabelProperty = AvaloniaProperty.Register<AnalyzerDashboard, string>(nameof(Label));
+    public static readonly StyledProperty<ICommand> RemoveCommandProperty = AvaloniaProperty.Register<GroupChip, ICommand>(nameof(RemoveCommand));
+    public static readonly StyledProperty<string> LabelProperty = AvaloniaProperty.Register<GroupChip, string>(nameof(Label));
 
     public ICommand RemoveCommand
     {
