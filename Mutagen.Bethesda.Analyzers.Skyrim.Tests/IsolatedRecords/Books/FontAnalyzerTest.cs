242be68171f8d26066dbaac6e44b8fbd052d96cc
Kieran <kieranknowles11@hotmail.co.uk>
2026-08-05 10:49:46 +0100

    Move theory data to testing framework

--- Mutagen.Bethesda.Analyzers.Skyrim.Tests/IsolatedRecords/Books/FontAnalyzerTest.cs
+++ Mutagen.Bethesda.Analyzers.Skyrim.Tests/IsolatedRecords/Books/FontAnalyzerTest.cs
@@ -1,26 +1,21 @@
-using AutoFixture.Xunit2;
 using Mutagen.Bethesda.Analyzers.Skyrim.Record.Book;
+using Mutagen.Bethesda.Analyzers.Testing.AutoFixture;
 using Mutagen.Bethesda.Analyzers.Testing.Frameworks;
 using Mutagen.Bethesda.Skyrim;
-using Mutagen.Bethesda.Testing.AutoData;
 using Xunit;
 
 namespace Mutagen.Bethesda.Analyzers.Skyrim.Tests.IsolatedRecords.Books;
 
 public class FontAnalyzerTest
 {
-    public class FontAnalyzerDataAttribute(params object[] objects) : CompositeDataAttribute(new InlineDataAttribute(objects), new MutagenModAutoDataAttribute())
-    {
-    }
-
     [Theory]
-    [FontAnalyzerData("'$BadFont'", "'$HandwrittenFont'")] // Font name must be valid
-    [FontAnalyzerData("'$BadFont'", "\"$HandwrittenFont\"")] // Single or double quotes are allowed
-    [FontAnalyzerData("'$BadFont'", "'$HandwrittenFont\"")] // Mismatched quote types are ok
-    [FontAnalyzerData("$HandwrittenFont", "'$HandwrittenFont'")] // Quotes are required
-    [FontAnalyzerData("'$HandwrittenFont", "'$HandwrittenFont'")] // Quotes must be closed
-    [FontAnalyzerData("'$BadFont'", "'$HANDWRITTENFONT'")] // Font names are case insensitve
-    [FontAnalyzerData("'HandwrittenFont'", "'$HandwrittenFont'")] // The $ is required
+    [MutagenModTheoryData("'$BadFont'", "'$HandwrittenFont'")] // Font name must be valid
+    [MutagenModTheoryData("'$BadFont'", "\"$HandwrittenFont\"")] // Single or double quotes are allowed
+    [MutagenModTheoryData("'$BadFont'", "'$HandwrittenFont\"")] // Mismatched quote types are ok
+    [MutagenModTheoryData("$HandwrittenFont", "'$HandwrittenFont'")] // Quotes are required
+    [MutagenModTheoryData("'$HandwrittenFont", "'$HandwrittenFont'")] // Quotes must be closed
+    [MutagenModTheoryData("'$BadFont'", "'$HANDWRITTENFONT'")] // Font names are case insensitve
+    [MutagenModTheoryData("'HandwrittenFont'", "'$HandwrittenFont'")] // The $ is required
     public void InvalidFont(string bad, string good, IsolatedRecordTestFixture<FontAnalyzer, Book, IBookGetter> fixture)
     {
         fixture.Run(
